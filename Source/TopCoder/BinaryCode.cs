835d0170396ace84455a728f15abad2829811f1c
Spawek <spawnerer@gmail.com>
2013-09-22 17:20:06 +0200

    added first test task done (BinaryCode)

---
+++ Source/TopCoder/BinaryCode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* start time: 15:55 */
+/* tests done: 16:00 */
+/* all basic test passed: 17:14 */
+/* 109.9 / 300 pts on submit */ //but how it works???
+public class BinaryCode
+{
+    public string[] decode(string inputString)
+    {
+        List<int> intputWith0AtStart = decodeToIntList(inputString, 0);
+        List<int> intputWith1AtStart = new List<int>(intputWith0AtStart);
+        intputWith1AtStart[1] = 1;
+
+        string input0Decoded = stringifyDecoded(decodeList(intputWith0AtStart));
+        string input1Decoded = stringifyDecoded(decodeList(intputWith1AtStart));
+
+        return new string[] 
+        { 
+            input0Decoded,
+            input1Decoded
+        };
+    }
+
+    private List<int> decodeToIntList(string inputString, int assummed1stSign)
+    {
+        List<int> input = new List<int>() { 0, assummed1stSign };
+        foreach (char item in inputString)
+        {
+            input.Add(Convert.ToInt32(item - '0'));
+        }
+
+        return input;
+    }
+
+    List<int> decodeList(List<int> input)
+    {
+        if (Math.Abs(input.Last() - input[input.Count - 2]) > 1)
+        {
+            return new List<int>();
+        }
+
+        List<int> decoded = new List<int>() { 0 };
+
+        decoded.Add(input[1]);
+        for (int i = 2; i < input.Count - 1; i++)
+        {
+            int newDecoded = input[i] - decoded[i - 2] - decoded[i - 1];
+            if (newDecoded != 0 && newDecoded != 1)
+            {
+                return new List<int>();
+            }
+            decoded.Add(newDecoded);
+        }
+
+        return decoded.GetRange(1, decoded.Count() - 1);
+    }
+
+    string stringifyDecoded(List<int> input)
+    {
+        if (input.Count == 0)
+        {
+            return "NONE";
+        }
+
+        StringBuilder sb = new StringBuilder(input.Count);
+        foreach (var item in input)
+        {
+            sb.Append(item.ToString());
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Source/TopCoderTests/BinaryCodeTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TopCoder;
+
+namespace TopCoderTests
+{
+    [TestClass]
+    public class BinaryCodeTests
+    {
+        BinaryCode binaryCode = new BinaryCode();
+
+        [TestMethod]
+        public void TestMethod0()
+        {
+            string input = "123210122";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "011100011", "NONE" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "11";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "01", "10" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = "22111";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "NONE", "11001" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string input = "123210120";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "NONE", "NONE" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string input = "3";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "NONE", "NONE" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string input = "12221112222221112221111111112221111";
+
+            string[] actual = binaryCode.decode(input);
+            string[] expected = new string[] { "01101001101101001101001001001101001",
+                "10110010110110010110010010010110010" };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
