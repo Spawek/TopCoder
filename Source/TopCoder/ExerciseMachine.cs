4aef5629e933632aea11a6a79ac667cd0f076200
Spawek <spawnerer@gmail.com>
2013-10-27 01:51:27 +0200

    added next task "exerciseMachine"

---
+++ Source/TopCoder/ExerciseMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * start: 01.29
+ * rozkmina zrobiona: 01.35
+ * testy zrobione: 01.39
+ * commit: 01.48
+ * 
+ * 359.00/500
+ */
+public class ExerciseMachine
+{
+    public int getPercentages(string time)
+    {
+        string[] splitted = time.Split(':');
+        int secs = Convert.ToInt32(splitted[0]) * 3600 + Convert.ToInt32(splitted[1]) * 60 + Convert.ToInt32(splitted[2]);
+
+        double interval = (double)secs / 100.0d;
+
+        int multipler = 0;
+        for (int i = 1; i < 100; i++)
+        {
+            if ((interval * i) % 1 < 0.001)
+            {
+                multipler = i;
+                break;
+            }
+        }
+
+        if (multipler == 0)
+        {
+            return 0;
+        }
+
+        return 100 / multipler - 1;
+    }
+}
---
+++ Source/TopCoderTests/ExerciseMachineTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoderTests
+{
+    [TestClass]
+    public class ExerciseMachineTests
+    {/*
+        [TestMethod]
+        public void TestMethod0()
+        {
+            ExerciseMachine em = new ExerciseMachine();
+
+            string input = "00:30:00";
+            int expected = 99;
+
+            int actual = em.getPercentages(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ExerciseMachine em = new ExerciseMachine();
+
+            string input = "00:28:00";
+            int expected = 19;
+
+            int actual = em.getPercentages(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ExerciseMachine em = new ExerciseMachine();
+
+            string input = "23:59:59";
+            int expected = 0;
+
+            int actual = em.getPercentages(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            ExerciseMachine em = new ExerciseMachine();
+
+            string input = "00:14:10";
+            int expected = 49;
+
+            int actual = em.getPercentages(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            ExerciseMachine em = new ExerciseMachine();
+
+            string input = "00:19:16";
+            int expected = 3;
+
+            int actual = em.getPercentages(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+      * */
+    }
+}
